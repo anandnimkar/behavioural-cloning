2ac943203432c818edf00092252baba089aed34f
Anand Nimkar <anand.a.nimkar@gmail.com>
2017-01-29 20:19:55 +0000

    wip

---
+++ model.py
@@ -0,0 +1,136 @@
+import argparse
+from keras.models import Sequential
+from keras.layers import Convolution2D, ELU, Lambda, Dropout, Flatten, Dense
+from keras.callbacks import EarlyStopping, ModelCheckpoint, 
+from sklearn.model_selection import train_test_split
+from data import load_data, shift_steering, generate
+from preprocess import preprocess_train, preprocess_test
+
+
+def load_data(data_folders):
+    dfs = []
+    for d in data_folders:
+        df = pd.read_csv(d + 'driving_log.csv') 
+        df.center = d + df.center.str.strip()
+        df.left = d + df.left.str.strip()
+        df.right = d + df.right.str.strip()
+        dfs.append(df.copy())
+    return pd.concat(dfs, ignore_index=True)
+
+
+def shift_steering(df, shift_value=0.25):
+    center = df[['center', 'steering']].rename(columns={'center': 'image_path'})
+    left = df[['left', 'steering']].rename(columns={'left': 'image_path'})
+    right = df[['right', 'steering']].rename(columns={'right': 'image_path'})
+    left['steering'] = np.minimum(left['steering'] + shift_value, 1.)
+    right['steering'] = np.maximum(right['steering'] - shift_value, -1.)
+    return pd.concat((center, left, right), ignore_index=True)
+
+
+def generate(df, preprocess_f, batch_size=128):
+    X, y = [], []
+    while 1:
+        df = shuffle(df)
+        for idx, row in df.iterrows():
+            image = cv2.imread(row['image_path'])
+            image, steering = preprocess_f(image, row['steering'])
+            image = image[None, :, :, :]
+            X.append(image)
+            y.append([steering])
+            if len(X) >= batch_size:
+                yield np.vstack(X), np.array(y)
+                X, y = [], []
+
+
+def get_model(im_shape=(66, 200, 3)):
+    """
+    Implementation of the nvidia model specified here: 
+    http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
+    
+    Concepts from the following model were also implemented:
+    https://github.com/commaai/research/blob/master/train_steering_model.py
+    """ 
+    model = Sequential()
+    
+    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=im_shape, output_shape=im_shape))
+    
+    model.add(Convolution2D(25, 5, 5, subsample=(2, 2), border_mode='valid', init='glorot_uniform'))
+    model.add(ELU())
+    
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode='valid', init='glorot_uniform'))
+    model.add(ELU())
+    
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid', init='glorot_uniform'))
+    model.add(ELU())
+    
+    model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='valid', init='glorot_uniform'))
+    model.add(ELU())
+    
+    model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='valid', init='glorot_uniform'))
+    model.add(Flatten())
+    model.add(Dropout(0.2))
+    model.add(ELU())
+    
+    model.add(Dense(50))
+    model.add(ELU())
+    model.add(Dropout(0.2))
+    
+    model.add(Dense(10))
+    model.add(ELU())
+    
+    model.add(Dense(1))
+    
+    model.compile(optimizer="adam", loss="mse", metrics=["accuracy"])
+
+    return model
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Steering angle model')
+    parser.add_argument('--data', nargs='+', default=['./data/sim_data/','./data/udacity_data/'], 
+                        help='Directories from which to load driving data')
+    parser.add_argument('--epochs', type=int, default=30, help='Number of epochs.')
+    parser.add_argument('--valid-split', type=float, default=0.25, help='Portion of total dataset to split for validation set')
+    parser.add_argument('--train-size', type=int, default=40000, help='Number of training samples per epoch.')
+    parser.add_argument('--valid-size', type=int, default=4000, help='Number of validation samples per epoch.')
+    args = parser.parse_args()
+    
+    df = load_data(args.data)
+    df = shift_steering(df)
+    df_train, df_valid = train_test_split(df, test_size=args.valid_split)
+    
+    print('{} training examples'.format(data_train.shape[0]))
+    print('{} validation examples'.format(data_valid.shape[0]))
+    
+    model = get_model()
+    model_name = 'model'
+    
+    # callbacks
+    stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=3, verbose=0, mode='min')
+    checkpoint = ModelCheckpoint(filepath='checkpoints/' + model_name + '.{epoch:02d}-{val_loss:.4f}',
+                                 monitor='val_loss', save_best_only=True, mode='min', save_weights_only=False)
+    
+    # generators
+    train_gen = generate(df_train, preprocess_f=preprocess_train)
+    valid_gen = generate(df_valid, preprocess_f=preprocess_test)
+    
+    
+    # model training and evaluation against validation set
+    model.fit_generator(train_gen, 
+                        samples_per_epoch=args.train_size, 
+                        nb_epochs=args.epochs,
+                        validation_data=valid_gen, 
+                        nb_val_samples=args.valid_size,
+                        callbacks=[stop, checkpoint])
+    
+    # saving model
+    print('Saving weights and model')
+    
+    with open('./{}.json'.format(model_name), 'w') as f:
+        f.write(model.to_json())
+        
+    model.save_weights('./{}.h5', True)
+    
+    
+    
+    
+    
--- preprocess.py
+++ preprocess.py
@@ -20,7 +20,7 @@ def bgr_to_rgb(image):
 def crop_roi(image, y1, y2, x1, x2):
     return image[y1:y2, x1:x2]
 
-def resize(image, new_x=200, new_y=66):
+def resize(image, new_x, new_y):
     return cv2.resize(image, dsize=(new_x, new_y))
 
 def threshold_steering(steer_value):
@@ -30,19 +30,19 @@ def threshold_steering(steer_value):
         return 1
     return steer_value
 
-def random_translation(image, steer_value, trans_range=40, steer_adjust=0.01):
+def random_translation(image, steer_value, trans_range, steer_adjust=0.01):
     x_shift = trans_range * np.random.uniform() - trans_range/2
     M = np.float32([[1, 0, x_shift], [0, 1, 0]])
     new_image = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
     new_steering = threshold_steering(steer_value + steer_adjust * x_shift)
     return new_image, new_steering
 
-def random_rotation(image, steer_value, angle_range=20, steer_multiplier=1.5):
+def random_rotation(image, steer_value, angle_range, steer_multiplier=1.5):
     r, c, _ = image.shape
     angle = np.random.uniform(angle_range) - angle_range/2
-    M = cv2.getRotationMatrix2D((c/2, r/2), angle, 1.0)
+    M = cv2.getRotationMatrix2D((c/2, r/2), -angle, 1.0) # angle is negated because it's more intuitive (i.e. positive == clockwise)
     new_image = cv2.warpAffine(image, M, (c, r), flags=cv2.INTER_LINEAR)
-    new_steering = threshold_steering(angle/90 * steer_multiplier) * -1
+    new_steering = threshold_steering(steer_value + (angle/90 * steer_multiplier))
     return new_image, new_steering
 
 def random_flip(image, steer_value):
@@ -53,8 +53,26 @@ def random_flip(image, steer_value):
 def random_brightness(image, a=0.2, b=1.5, return_hsv=True):
     hsv = bgr_to_hsv(image)
     factor = np.random.uniform(a, b)
-    print(factor)
     hsv[...,2] = np.minimum(hsv[...,2] * factor, 255)
     if return_hsv:
         return hsv
     return hsv_to_bgr(hsv)
+
+def preprocess_train(image, steer_value, trans_range=40, angle_range=20):
+    image, steer_value = random_translation(image, steer_value, trans_range)
+    image, steer_value = random_rotation(image, steer_value, angle_range)
+    image, steer_value = random_flip(image, steer_value)
+    image = random_brightness(image)
+    image = crop_roi(image, 35, 135, int(trans_range/2), int(image.shape[1] - trans_range/2))
+    image = resize(image, 200, 66)
+    return image, steer_value
+
+def preprocess_test(image, steer_value, rgb=False, trans_range=40):
+    if rgb:
+        image = rgb_to_hsv(image)
+    else:
+        image = bgr_to_hsv(image)
+    image = crop_roi(image, 35, 135, int(trans_range/2), int(image.shape[1] - trans_range/2))
+    image = resize(image, 200, 66)
+    return image, steer_value
+    
